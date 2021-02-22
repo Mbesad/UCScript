1fbe1f440373424782f4306e322380524c633716
Mbesad <77247077+Mbesad@users.noreply.github.com>
2021-02-22 19:16:21 -0300

    Update UCscript.ps1

--- UCscript.ps1
+++ UCscript.ps1
@@ -1,1 +1,52 @@
-#### CODE 1.0.0 ####
+
+####################### VERSION CHECK (do not remove) ###################################################
+Write-Output "Checking for version updates..."
+$skip_update = $false
+# Retrieve status file from Github 
+$stat = Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Mbesad/UCScript/main/UCScript_status" -TimeoutSec 10
+$content = $stat.Content
+$content = $content.split([Environment]::NewLine)
+$newver = $null
+# Search for script version in the status file
+ForEach ($element in $content)
+{
+    if ($element -like "Version=*"){$newver = $element.split('=')[1]} 
+}
+If ($newver -eq $null)
+{
+Write-Output "Could not get current version on Github. Continuing..."
+$skip_update = $true
+}
+# Retrieve status file from local machine
+$curr_status_file = Get-Content .\UCScript_status.txt -Raw
+$curr_status_file = $curr_status_file.split([Environment]::NewLine)
+# Search for script version in the status file
+ForEach ($element in $curr_status_file)
+{
+    if ($element -like "Version=*"){$currver = $element.split('=')[1]} 
+}
+If ($currver -eq $null){
+Write-Output "Could not get current version on local machine. Continuing..."
+$skip_update = $true
+}
+
+#Compare versions#
+if (!$skip_update)
+{
+$newver = $newver.split('.')
+$currver = $currver.split('.')
+if ($newver[0] -lt $currver[0]) {$skip_update = $true}
+if (($newver[0] -eq $currver[0]) -And ($newver[1] -lt $currver[1])) {$skip_update = $true}
+if (($newver[0] -eq $currver[0]) -And ($newver[1] -eq $currver[1]) -And ($newver[2] -le $currver[2])) {$skip_update = $true}
+}
+
+
+if (!$skip_update)
+{
+$script_file = Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Mbesad/UCScript/main/UCScript.ps1" -TimeoutSec 10 
+$code = $script_file.Content
+
+###################################################################################################################
+################################################ SCRIPT BEGINS ####################################################
+
+## CODE 2.0
